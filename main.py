7c5d0226d5f4f5df915b67f39c948f2f4ac9da76
KajitaTakaya <takaya0618.kjt@gmail.com>
2025-06-02 18:50:46 +0900

    portfording

--- main.py
+++ main.py
@@ -371,4 +371,5 @@ def toggle_like():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True, host='192.168.1.173', port=25575)  #kjserver公開用
+    app.run(debug=True) 
