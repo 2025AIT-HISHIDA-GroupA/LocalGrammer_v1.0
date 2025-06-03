fb5cdfd54b8e5788fc9f80a8aae1c7cf349a3c98
KajitaTakaya <takaya0618.kjt@gmail.com>
2025-06-03 10:50:17 +0900

    写真の追加

--- main.py
+++ main.py
@@ -372,4 +372,4 @@ def toggle_like():
 
 if __name__ == '__main__':
     #app.run(debug=True, host='192.168.1.173', port=25575)  #kjserver公開用
-    app.run(debug=True) 
+    app.run(debug=True,host='0.0.0.0', port=25575)
